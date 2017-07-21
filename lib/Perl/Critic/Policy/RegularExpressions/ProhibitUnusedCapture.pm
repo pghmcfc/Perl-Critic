55a4ebac52700e2d7c379c1bbbd1d6acf3f45dac
Paul Howarth <paul@city-fan.org>
2017-07-21 11:50:35 +0100

    Change default spell check tool from aspell to hunspell
    This is on the basis that most downstream users are already doing
    or have migrated from aspell to hunspell due to hunspell being
    actively maintained, unlike aspell.
    
    https://wiki.ubuntu.com/ConsolidateSpellingLibs
    https://wiki.gnome.org/Initiatives/SpellChecking
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=860895
    https://fedoraproject.org/wiki/Releases/FeatureDictionary
    
    Unfortunately, hunspell prior to about version 1.2.12 (2010) had
    problems with ASCII apostrophes, thus flagging errors for words
    like "doesn't", "isn't" etc., which makes 1.2.12 pretty much a
    minimum version requirement if incorporating this change.

--- lib/Perl/Critic/Policy/RegularExpressions/ProhibitUnusedCapture.pm
+++ lib/Perl/Critic/Policy/RegularExpressions/ProhibitUnusedCapture.pm
@@ -764,7 +764,7 @@ __END__
 
 =pod
 
-=for stopwords refactored
+=for stopwords refactored regexp
 
 =head1 NAME
 
--- lib/Perl/Critic/Utils.pm
+++ lib/Perl/Critic/Utils.pm
@@ -1388,6 +1388,8 @@ __END__
 
 =pod
 
+=for stopwords foo
+
 =head1 NAME
 
 Perl::Critic::Utils - General utility subroutines and constants for Perl::Critic and derivative distributions.
